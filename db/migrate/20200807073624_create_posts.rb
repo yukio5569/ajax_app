95456f6383cbf38da875814b93f6af55ad86c0af
yukio5569 <pekapeka777@icloud.com>
2020-08-28 12:12:50 +0900

    AjaxAppの準備

--- db/migrate/20200807073624_create_posts.rb
+++ db/migrate/20200807073624_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.text :memo
+      t.text :content
+      t.boolean :checked
       t.timestamps
     end
   end
